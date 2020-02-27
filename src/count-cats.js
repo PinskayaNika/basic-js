abce7f4e78c0bedf6ab624db151024000f8f6347
PinskayaNika <56188948+PinskayaNika@users.noreply.github.com>
2020-02-28 01:13:24 +0300

    feature: count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,16 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+module.exports = function countCats( matrix ) {
+  //throw 'Not implemented';
   // remove line with error and write your code here
-};
+    let count = 0;
+    for (let i = 0; i < matrix.length; i++) {
+    	for(let j = 0; j < matrix[i].length; j++) {
+        	if (String(matrix[i][j]).length == 2) {
+            	if (String(matrix[i][j]).substr(0,1) == '^' && String(matrix[i][j]).substr(1,1) == '^') {
+            		count = count + 1;
+                }
+            }
+        }
+     }
+     return count;
+   
+}
