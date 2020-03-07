b2a1b6d86d45f10a38169e75cb4fff7501fe8af0
PinskayaNika <56188948+PinskayaNika@users.noreply.github.com>
2020-03-07 23:07:16 +0300

    feature: add dream team creator

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,30 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members ) {
+	name = '';
+	team = [];
+	let pos = 0;
+    if (typeof(members) != "object" || !Array.isArray(members)) {
+        	return false;
+        } else {
+	for (let i = 0; i < members.length; i++) {
+	if(members.length == 0) {
+    return '';
+    } else	if (typeof(members[i]) != "string") {
+        members[i] = '';
+        }
+        members[i] = members[i].trim().toUpperCase();
+    	/*if (members[i].substring(pos,1) == ' ') {
+    		members[i] = '';
+        	pos = pos + 1;
+    	}*/
+    }
+	team = members.sort();
+	//return team;
+	for (let i = 0; i < members.length; i++) {
+    	
+    	name = name + team[i].substring(0,1);
+    }
+    return name;
+    }
+  	//throw 'Not implemented';
+  	// remove line with error and write your code here
 };
