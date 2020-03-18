c7e5128fb0100ef5fe73c2655a1a22921673c11e
PinskayaNika <56188948+PinskayaNika@users.noreply.github.com>
2020-03-18 23:48:22 +0300

    feature: add class DepthCalculator

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,18 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
+    calculateDepth( arr ) {
+		let depth = 1;
+        ///throw 'Not implemented';
         // remove line with error and write your code here
-    }
+		if (typeof(arr) != "object") {
+			return 0;
+		}   	
+		for (let i = 0; i < arr.length; i++) {
+			//if (arr[i] == "[") {
+				depth = Math.max(this.calculateDepth(arr[i]) + 1, depth);
+			//} 
+		//return depth;
+			//return arr.reduce(); 
+		}
+	return depth;
+	}
 };
