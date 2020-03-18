1b3cbbddc079a7724eff279c60bc1c4f52c143e5
PinskayaNika <56188948+PinskayaNika@users.noreply.github.com>
2020-03-18 22:47:07 +0300

    feature: add calculateHonoi function

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,15 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
+module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
+    //throw 'Not implemented';
     // remove line with error and write your code here
+	let turns = 0.0;
+    let seconds = 0.0;
+    
+    turns = Math.pow(2, disksNumber) - 1 ;
+    seconds = turns / (turnsSpeed / 3600);
+//return turns;
+//return seconds;
+	return {
+    	turns: turns,
+      seconds: seconds
+   }
 }
