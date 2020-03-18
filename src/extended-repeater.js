012d65c2d3688fcb4b30fae38322c3c349fc787d
PinskayaNika <56188948+PinskayaNika@users.noreply.github.com>
2020-03-19 01:09:45 +0300

    feature: add extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,44 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
+module.exports = function repeater( str, options ) {
+    //throw 'Not implemented';
     // remove line with error and write your code here
+	let separator = '';
+	let additionSeparator = '';
+	let repeatTimes = 0;
+	let additionRepeatTimes = 0;
+	let addition = 0;
+	
+	let result = '';
+
+	if (options.repeatTimes == undefined) {
+		options.repeatTimes = 1;
+	}
+	if (options.separator == undefined) {
+		options.separator = '+';
+	}
+	if (options.addition === undefined) {
+		options.additionRepeatTimes = 0;
+	}
+	if (options.additionRepeatTimes == undefined) {
+		options.additionRepeatTimes = 1;
+	}
+	
+	if (options.additionSeparator == undefined) {
+		options.additionSeparator = '|';
+	}
+
+	for (let i = 0; i < options.repeatTimes; i++) {
+		result = result + str;
+		for (let j = 0; j < options.additionRepeatTimes; j++) {
+			result = result + options.addition;
+			if (j < options.additionRepeatTimes - 1) {
+				result = result + options.additionSeparator;
+			}
+		}
+		if (i < options.repeatTimes - 1) {
+			result = result + options.separator;
+		}
+	}
+	
+	return result;
 };
   
